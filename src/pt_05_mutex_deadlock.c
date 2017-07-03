5e50d33c019c3fcd5d771888d4474111259ed566
Jonathan Price <jlprice@umich.edu>
2017-07-02 21:37:53 -0400

    refactored pt_05

--- src/pt_05_mutex_deadlock.c
+++ src/pt_05_mutex_deadlock.c
@@ -20,36 +20,32 @@
 pthread_mutex_t Lock1;
 pthread_mutex_t Lock2;
 
-/** 
- * Simple struct to use for an argument for the newly
- * created threads.  Obviously any data will work within
- * the struct, this is just an illustrative example.
- */
-typedef struct tinfo_s {
-    int thread_num;
-    char buf[80];
-} tinfo_t;
-
 /**
- * Generic thread start routine
+ * Thread1 - will take Lock1, sleep to yield to Thread2,
+ *           then try to take Lock2.
  */
 void*
-thread_func(void *arg) 
+thread1_func(void *arg) 
 {
-    tinfo_t *tmp = (tinfo_t*)arg;
+    printf("Thread 1 taking Lock1, sleeping 5 to yield to Thread2\n");
+    pthread_mutex_lock(&Lock1);
+    sleep(5);
+    printf("Thread 1 awoken, trying to take Lock2, will deadlock...\n");
+    pthread_mutex_lock(&Lock2);
 
-    if (1 == tmp->thread_num) {
-        printf("Thread 1 taking Lock1, sleeping 5 to yield to Thread2\n");
-        pthread_mutex_lock(&Lock1);
-        sleep(5);
-        printf("Thread 1 awaken, trying to take Lock2, will deadlock...\n");
-        pthread_mutex_lock(&Lock2);
-    }
-    if (2 == tmp->thread_num) {
-        printf("Thread 2 taking Lock2 then Lock1, which will block\n");
-        pthread_mutex_lock(&Lock2);
-        pthread_mutex_lock(&Lock1);
-    }
+    pthread_exit(NULL);
+}
+
+/**
+ * Thread2 - Will take Lock1, then try to take Lock1, which will block
+ */
+void*
+thread2_func(void *arg) 
+{
+    printf("Thread 2 taking Lock2 then Lock1, which will block\n");
+
+    pthread_mutex_lock(&Lock2);
+    pthread_mutex_lock(&Lock1);
 
     pthread_exit(NULL);
 }
@@ -70,18 +66,11 @@ int main()
         return 0;
     }
 
-    /* Create the args we will pass to each thread */
-    tinfo_t thread_args[MAX_THREADS];
-    for (i = 0; i < MAX_THREADS; i++) {
-        thread_args[i].thread_num = i+1;
-        sprintf(thread_args[i].buf, "Thread Number: %u", i);
-    }
-
     /* Create the thread ids */
     pthread_t tids[MAX_THREADS] = { 0 };
 
     /* Create the first thread, give it a couple seconds to run */
-    if (0 == pthread_create(&tids[0], NULL, thread_func, (void*)&thread_args[0])) {
+    if (0 == pthread_create(&tids[0], NULL, thread1_func, NULL)) {
         printf("Thread 1 created, sleeping for a couple seconds to allow it to run before creating Thread2\n");
         sleep(2);
     } else {
@@ -89,7 +78,7 @@ int main()
     }
     
     /* Create the second thread */
-    if (0 == pthread_create(&tids[1], NULL, thread_func, (void*)&thread_args[1])) {
+    if (0 == pthread_create(&tids[1], NULL, thread2_func, NULL)) {
         printf("Thread 2 created\n");
     } else {
         printf("Thread 2 create failed: ret=%u\n", ret);
